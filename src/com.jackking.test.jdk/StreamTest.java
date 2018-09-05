b57348935a40f42e84635498dc19b67061f3de0d
e604847 <jjin4@statestreet.com>
2018-09-05 08:51:52 +0800

    Add idea setting file

---
+++ src/com.jackking.test.jdk/StreamTest.java
@@ -0,0 +1,15 @@
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Stream;
+
+/**
+ * Created by e604847 on 9/4/2018.
+ */
+public class StreamTest {
+    public static void main(String[] args) {
+        String[] s = {"aaa","bbb","ccc"};
+        List<String> list = Arrays.asList(s);
+        Stream<String> lStream=list.stream();
+        lStream.filter(string -> !string.contains("s")).forEach(System.out::println);
+    }
+}
